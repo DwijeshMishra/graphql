8bd624bafac5730aa0bf658b604947f51c8f7d09
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2022-06-10 18:56:28 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,7 +8,8 @@ do
     cd ..
     cp project-specific.yaml $repo/  && cp .repo-config  $repo/
     cd $repo
-    git 
+    git config --global user.name "github runner"
+    git config --global user.email "github@gmail.com"
     git add . 
     git commit -am"update workflow"
     git push
