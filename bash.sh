c9af75948794d5653f8e015bbf3cf7de8a522a37
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2022-06-10 19:39:06 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,7 +9,10 @@ do
     cp project-specific.yaml $repo/  && cp .repo-config  $repo/
     cd $repo
     git add . 
+    git config --global user.name "DwijeshMishra"
+    git config --global user.email "dwijeshm3@gmail.com"
     git commit --amend --reset-author
+    git commit --amend --author='DwijeshMishra <dwijeshm3@gmail.com>'
     git commit -am"update workflow"
     git push --set-upstream origin feature
 done < java-repos.txt
