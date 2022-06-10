b86b0778c6ba17424ec76f76b9b6e3b48f5d2ccb
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2022-06-10 20:36:26 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,8 +2,9 @@ FILENAME="java-repos.txt"
 
 while read -r repo
 do
-    git clone https://github.com/DwijeshMishra/$repo
+    git clone https://DwijeshMishra:$secrets.API_TOKEN_GITHUB@github.com/DwijeshMishra/$repo.git
     cd $repo
+    git remote set-url origin https://DwijeshMishra:$secrets.API_TOKEN_GITHUB@github.com/DwijeshMishra/$repo.git
     git checkout -b feature
     cd ..
     cp project-specific.yaml $repo/  && cp .repo-config  $repo/
@@ -12,6 +13,6 @@ do
     git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
     git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
     git commit -am"update workflow"
-    git push --set-upstream origin feature
+    git push --set-upstream origin feature 
     cat .git/config
 done < java-repos.txt
