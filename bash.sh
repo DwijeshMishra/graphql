87c814e84788fe0c2cb8c86cb25cc3b12d52ef40
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2022-06-10 19:36:39 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,6 +9,7 @@ do
     cp project-specific.yaml $repo/  && cp .repo-config  $repo/
     cd $repo
     git add . 
+    git commit --amend --reset-author
     git commit -am"update workflow"
     git push --set-upstream origin feature
 done < java-repos.txt
