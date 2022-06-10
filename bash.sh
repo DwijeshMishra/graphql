0bafda350379dfaddd7898015982d97c8bf4c2eb
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2022-06-10 21:06:34 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,7 +4,7 @@ while read -r repo
 do
     git clone https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/DwijeshMishra/$repo.git
     cd $repo
-    git remote set-url origin https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/DwijeshMishra/$repo.git
+    git remote set-url origin https://DwijeshMishra:$GITHUB_TOKEN@github.com/DwijeshMishra/$repo.git
     git checkout -b feature
     cd ..
     cp project-specific.yaml $repo/  && cp .repo-config  $repo/
@@ -13,7 +13,7 @@ do
     git config user.name "DwijeshMishra"
     git config user.email "dwijeshm3@gmail.com"
     git commit -am"update workflow"
-    git remote add origin https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/DwijeshMishra/$repo.git
+    git remote add origin https://DwijeshMishra:$GITHUB_TOKEN@github.com/DwijeshMishra/$repo.git
     git push -u origin feature
     cat .git/config
 done < java-repos.txt 
