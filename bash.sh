feec22fc260eff36ddee2b15c153c5ead0c0c9b0
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2022-06-10 22:29:09 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,9 +2,9 @@ FILENAME="java-repos.txt"
 
 while read -r repo
 do
-    git clone https://DwijeshMishra:$api_token@github.com/DwijeshMishra/$repo.git
+    git clone https://DwijeshMishra:$API_TOKEN@github.com/DwijeshMishra/$repo.git
     cd $repo
-    git remote set-url origin https://DwijeshMishra:$apt_token@github.com/DwijeshMishra/$repo.git
+    git remote set-url origin https://DwijeshMishra:$API_TOKEN@github.com/DwijeshMishra/$repo.git
     git checkout -b feature
     cd ..
     cp project-specific.yaml $repo/  && cp .repo-config  $repo/
